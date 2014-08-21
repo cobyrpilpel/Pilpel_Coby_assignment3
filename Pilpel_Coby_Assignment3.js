357ad5fdcf293e4f08ef3a726654c12f9e2ebfb9
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-08-21 13:43:12 -0400

    First commit
    vars done

---
+++ Pilpel_Coby_Assignment3.js
@@ -0,0 +1,24 @@
+//alert("JavaScript works!");
+
+// Coby Pilpel
+//SDI 1408
+//Project 3
+
+
+//Variables
+
+var partsSetOne = ["servos", "control surfaces", "wheels", "propeller blades"];
+
+var partsSetTwo = ["frame", "radio parts", "body coverings", "battery"];
+
+var numberLeftOverScrews = 10
+var chargeUpTime = 25
+var glueTime = 5
+var setTime = 2
+var areWheelsAttached = true
+var didGlueSet = true
+var allPartsSecure = true
+var timeHeatGun = 2 
+
+
+var allSuppliesOrganized = confirm("Are all the parts, supplies and tools organized?")
